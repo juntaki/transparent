910bd2d11676650ae0748ece7902e293c3fd8ed0
juntaki <me@juntaki.com>
2016-11-22 18:35:01 +0900

    Fix comment

--- transparent.go
+++ transparent.go
@@ -1,7 +1,6 @@
-// Package transparent A transparent package is a library that provides
-// transparent caching operations for key-value stores. As shown in the figure
-// below, it is possible to use relatively fast cache like LRU and slow
-// and reliable storage like S3 via TransparentCache.
+// Package transparent is a library that provides transparent caching operations
+// for key-value stores. As shown in the figure below, it is possible to use relatively
+// fast cache like LRU and slow and reliable storage like S3 via TransparentCache.
 // Transparent Cache is tearable. In addition to caching, it is also possible to
 // transparently use a layer of synchronization between distributed systems.
 // See subpackage for example.
