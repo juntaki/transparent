fbe8231e8bd55dacdad429199fb3972c0fc7e2a7
juntaki <me@juntaki.com>
2016-12-22 12:42:45 +0900

    Fix example test

--- example_test.go
+++ example_test.go
@@ -1,30 +1,50 @@
 package transparent_test
 
-// func Example() {
-// 	var err error
-// 	cacheLayer1, _ := transparent.NewLRUCache(10, 100)
-// 	cacheLayer2, _ := transparent.NewFilesystemCache(10, "/tmp")
-// 	sourceLayer, _ := transparent.NewDummySource(10)
-// 	transparent.Stack(cacheLayer1, cacheLayer2)
-// 	transparent.Stack(cacheLayer2, sourceLayer)
+import (
+	"fmt"
 
-// 	cacheLayer1.Set("key", []byte("value"))
-// 	cacheLayer1.Sync()
-// 	value, _ := cacheLayer1.Get("key")
-// 	fmt.Printf("%s\n", value)
+	"github.com/juntaki/transparent"
+	"github.com/juntaki/transparent/filesystem"
+	"github.com/juntaki/transparent/lru"
+	"github.com/juntaki/transparent/test"
+)
 
-// 	value, err = cacheLayer2.Get("key")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Printf("%s\n", value)
-// 	value, err = sourceLayer.Get("key")
-// 	if err != nil {
-// 		fmt.Println(err)
-// 	}
-// 	fmt.Printf("%s\n", value)
-// 	// Output:
-// 	// value
-// 	// value
-// 	// value
-// }
+func Example() {
+	var err error
+	cacheLayer1, _ := lru.NewCache(10, 100)
+	cacheLayer2 := filesystem.NewCache(10, "/tmp")
+	sourceLayer := test.NewSource(10)
+
+	stack := transparent.NewStack()
+	stack.Stack(sourceLayer)
+	stack.Stack(cacheLayer2)
+	stack.Stack(cacheLayer1)
+
+	stack.Start()
+	defer stack.Stop()
+
+	stack.Set("key", []byte("value"))
+	stack.Sync()
+
+	value, _ := stack.Get("key")
+	fmt.Printf("%s\n", value)
+
+	value, _ = cacheLayer1.Get("key")
+	fmt.Printf("%s\n", value)
+
+	value, err = cacheLayer2.Get("key")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", value)
+	value, err = sourceLayer.Get("key")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Printf("%s\n", value)
+	// Output:
+	// value
+	// value
+	// value
+	// value
+}
